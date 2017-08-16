c494632478197e84d259790ca8d210a98144de97
jack <e.kobets@talantarium.com>
2017-08-16 19:24:47 +0300

    ConstructorWithExceptions

--- src/ConstructorWithExceptions.java
+++ src/ConstructorWithExceptions.java
@@ -6,12 +6,14 @@ public class ConstructorWithExceptions {
 
         public Parent() throws SQLException {
         }
+        public void method() throws Exception {
+        }
     }
-
     public class Child extends Parent {
 
-        public Child() throws Exception {
-            // У наследника или такой же или более широкий exception
+        public Child() throws Exception {// У наследника или такой же или более широкий exception
+        }
+        public void method() throws SQLException {// У наследника или меньше или такой же.
         }
     }
 }
